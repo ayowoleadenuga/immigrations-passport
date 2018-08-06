e04498043eabaca566a35a75a3b76b09b0f799e0
Ayowole <ayowoleadenuga@gmail.com>
2018-08-06 02:51:30 +0100

    script added

--- main.js
+++ main.js
@@ -1,12 +1,45 @@
 // assigning all variables 
-var home = document.querySelector(".home");
-var about = document.querySelector(".about");
-var contact = document.querySelector(".contact-Us")
+let home = document.querySelector(".home");
+let about = document.querySelector(".about");
+let contact = document.querySelector(".contact-Us");
+let body = document.querySelector("body");
+let nav = document.querySelector(".map");
 
-var toggle = function(element){
-    location.reload();
-    home.style.display === "none";
-    if (element.classList.contains('hidden')){
-        this.classList.remove('hidden');
-    }
-} 
+let changeDisplayHome = () => {
+  Show(home);
+  Hide(about, contact);
+  home.className = "home";
+};
+let changeDisplayAbout = () => {
+  Show(about);
+  Hide(home, contact);
+};
+let changeDisplayContact = () => {
+  Show(contact);
+  Hide(about, home);
+};
+// let changeBackgroundBody = () => {
+//     if(this.className = "con") {
+//         body.style.background = "#2E4316";
+//     } else {
+//         body.style.background = null;
+//         // changeBackgroundBody() = false;
+//     }
+// };
+// let changeBackgroundMap = () => {
+//     if(this.classList = "abt"){
+//         map.style.background = "#2E4316";
+//     } else {
+//         map.style.background = null;
+//         // changeBackgroundBody() = false;
+//     }
+// };
+
+let Show = class1 => {
+  class1.className = "show";
+};
+
+let Hide = (class2, class3) => {
+  class2.className = "hidden";
+  class3.className = "hidden";
+};
