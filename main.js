9b27675b6b14fc1b93fc5acef6b6bd01f2a0f577
Ayowole <ayowoleadenuga@gmail.com>
2018-08-07 12:40:40 +0100

    final

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-// assigning all variables 
+// assigning all variables
 let home = document.querySelector(".home");
 let about = document.querySelector(".about");
 let contact = document.querySelector(".contact-Us");
 let body = document.querySelector("body");
-let nav = document.querySelector(".map");
+let map = document.querySelector(".map");
 
 let changeDisplayHome = () => {
+  window.location.reload();
   Show(home);
   Hide(about, contact);
   home.className = "home";
@@ -18,22 +19,24 @@ let changeDisplayContact = () => {
   Show(contact);
   Hide(about, home);
 };
-// let changeBackgroundBody = () => {
-//     if(this.className = "con") {
-//         body.style.background = "#2E4316";
-//     } else {
-//         body.style.background = null;
-//         // changeBackgroundBody() = false;
-//     }
-// };
-// let changeBackgroundMap = () => {
-//     if(this.classList = "abt"){
-//         map.style.background = "#2E4316";
-//     } else {
-//         map.style.background = null;
-//         // changeBackgroundBody() = false;
-//     }
-// };
+let changeBackgroundBody = () => {
+    if(document.querySelector('.con').className = "con") {
+        body.style.background = "#2E4316";
+     } // else {
+    //     alert('sorry');
+    //     body.style.background = null;
+    //     // changeBackgroundBody() = false;
+    // }
+}
+let changeBackgroundMap = () => {
+    if(document.querySelector('.abt').classList = "abt"){
+        map.style.background = "#2E4316";
+        body.style.background = 'url(https://res.cloudinary.com/ayowoleadenuga/image/upload/v1533319808/nigeria-ecowas-passports.png) no-repeat center center fixed';
+     } // else {
+    //     map.style.background = null;
+    //     // changeBackgroundBody() = false;
+    // }
+};
 
 let Show = class1 => {
   class1.className = "show";
